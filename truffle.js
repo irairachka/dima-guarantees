344a54020bac0a64b84bb4899c496e0b2ba67198
irairachka <iraeid123@gmail.com>
2018-05-14 19:28:37 +0300

    package.json fixis

--- truffle.js
+++ truffle.js
@@ -5,11 +5,13 @@ var request = require('request')
 module.exports = {
   networks: {
     development: {
-       host: '127.0.0.1',
+      host: '127.0.0.1',
+	  //host: '51.144.177.48',
       //host: 'ec2-52-59-221-203.eu-central-1.compute.amazonaws.com' ,
-      port: 8545,
+      port: 22000,
       network_id: '*',// Match any network id
-      gas: 57000000
+      gas: 57000000,
+      gasPrice: 0
     }
   }
 };
